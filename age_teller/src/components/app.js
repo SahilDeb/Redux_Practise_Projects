7477c8df2eb3e58e022904ffc62ca7f46649e374
Sahil Debnath <sahil.debnath@inszoom.com>
2020-07-25 22:15:06 +0530

    AgeTeller Complete

--- age_teller/src/components/app.js
+++ age_teller/src/components/app.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
 import { Form, Button, FormControl } from 'react-bootstrap';
 
+import './App.css';
+
+import AgeStats from './AgeStats';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: "",
+      birthday: '1992-05-21',
+      showStats: false
+    };
+  }
+
+  changeBirthday() {
+    this.setState({ birthday: this.state.newDate, showStats: true })
+  }
   render() {
     return (
-      <div>
+      <div className="App">
         <Form inline>
-          <FormControl type="date">
+          <h2>Input your birthday!</h2>
+          <FormControl 
+            type="date" 
+            onChange={ event => this.setState({ newDate: event.target.value })}>
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()}>
             Submit
           </Button>
+          {
+            this.state.showStats ?
+            <div className="fade age-stats">
+              <AgeStats date={this.state.birthday}/>
+            </div>
+            : null
+          }
         </Form>
       </div>
     )
